c126fb5abee25ed7674962f0aca89da83427dab9
Sai Kiran Dasika <kirandasika30@gmail.com>
2018-10-20 05:54:11 -0400

    algorithm ml

--- main.py
+++ main.py
@@ -3,6 +3,55 @@ import cv2
 import sys
 import numpy as np
 
+face_cascade = cv2.CascadeClassifier("./algorithm.xml")
+p0 = None
+face_center = None
+
+def distance(x, y):
+    return np.linalg.norm(a-b)
+
+
+#params for ShiTomasi corner detection
+feature_params = dict( maxCorners = 100,
+                        qualityLevel = 0.3,
+                        minDistance = 7,
+                        blockSize = 7 )
+
+lk_params = dict( winSize  = (15,15),
+                  maxLevel = 2,
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+def get_coords(p1):
+    try: return int(p1[0][0][0]), int(p1[0][0][1])
+    except: return int(p1[0][0]), int(p1[0][1])
+
+
+max_head_movement = 20
+movement_threshold = 50
+gesture_threshold = 175
+
+def find_face():
+    #find the face in the image
+    face_found = False
+    frame_num = 0
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    while frame_num < 30:
+        # Take first frame and find corners in it
+        frame_num += 1
+        ret, frame = cap.read()
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(frame_gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            face_found = True
+        cv2.imshow('image',frame)
+        cv2.waitKey(1)
+    face_center = x+w/2, y+h/3
+    p0 = np.array([[face_center]], np.float32)
+    cv2.destroyAllWindows()
+
 class WebcamVideoStream :
     def __init__(self, src = 0, width = 320, height = 240) :
         self.stream = cv2.VideoCapture(src)
@@ -42,12 +91,24 @@ class WebcamVideoStream :
         self.stream.release()
 
 if __name__ == "__main__" :
+    find_face()
     vs = WebcamVideoStream().start()
+    print("Face center: {}".format(p0))
     while True :
         frame = vs.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
     vs.stop()
     cv2.destroyAllWindows()
