08a47f5c8d8e9f9fd4062ab0ca7875fedbe8ac79
Taric Jain <tzj0019@auburn.edu>
2019-11-02 16:33:02 -0400

    updated dependencies

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import time
+
 import cv2
 import sys
+import os
 import socket
 import json
+import math
+from dotenv import load_dotenv
 import numpy as np
 from threading import Thread, Lock
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+import cognitive_face as CF
 
+load_dotenv()
+KEY = os.getenv('FACE_API_SUBSCRIPTION_KEY')
+ENDPOINT = os.getenv('FACE_API_ENDPOINT')
 
+CF.Key.set(KEY)
+CF.BaseUrl.set(ENDPOINT)
 face_cascade = cv2.CascadeClassifier("./algorithm.xml")
+
 p0 = None
 face_center = None
 frame_gray = None
@@ -14,106 +28,33 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 gesture = False
 x_movement = 0
 y_movement = 0
-gesture_show = 10 #number of frames a gesture is shown
+gesture_show = 10  # number of frames a gesture is shown
+max_head_movement = 20
+movement_threshold = 50
+gesture_threshold = 150
 # gesture_lock is used when a gesture is recorded and an action has to be taken based on it
 gesture_lock = Lock()
 handling_action = False
+# face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
-def distance(x,y):
-    import math
-    return math.sqrt((x[0]-y[0])**2+(x[1]-y[1])**2)
 
-#params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100,
-                        qualityLevel = 0.3,
-                        minDistance = 7,
-                        blockSize = 7 )
+# params for ShiTomasi corner detection
+feature_params = dict(maxCorners=100, qualityLevel=0.3, minDistance=7, blockSize=7)
+lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 2,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
 def get_coords(p1):
     try: return int(p1[0][0][0]), int(p1[0][0][1])
     except: return int(p1[0][0]), int(p1[0][1])
 
 
-max_head_movement = 20
-movement_threshold = 50
-gesture_threshold = 150
+def distance(x, y):
+    return math.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)
 
-def find_face():
-    try:
-        #find the face in the image
-        face_found = False
-        frame_num = 0
-        cap = cv2.VideoCapture(0)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-        while frame_num < 30:
-            # Take first frame and find corners in it
-            frame_num += 1
-            ret, frame = cap.read()
-            global frame_gray
-            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # faces = face_cascade.detectMultiScale(frame_gray, 1.3, 5)
-            faces = face_cascade.detectMultiScale(frame_gray,
-                                    scaleFactor=1.1,
-                                    minNeighbors=5,
-                                    minSize=(30,30),
-                                    flags=cv2.CASCADE_SCALE_IMAGE)
-            for (x,y,w,h) in faces:
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-                face_found = True
-            cv2.imshow('image',frame)
-            cv2.waitKey(1)
-        global face_center
-        face_center = x+w/2, y+h/3
-        global p0
-        p0 = np.array([[face_center]], np.float32)
-    except:
-        sys.exit(1)
 
-class WebcamVideoStream :
-    def __init__(self, src = 0, width = 320, height = 240) :
-        self.stream = cv2.VideoCapture(src)
-        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        (self.grabbed, self.frame) = self.stream.read()
-        self.started = False
-        self.read_lock = Lock()
-
-    def start(self) :
-        if self.started:
-            print("already started!!")
-            return None
-        self.started = True
-        self.thread = Thread(target=self.update, args=())
-        self.thread.start()
-        return self
-
-    def update(self) :
-        while self.started :
-            (grabbed, frame) = self.stream.read()
-            self.read_lock.acquire()
-            self.grabbed, self.frame = grabbed, frame
-            self.read_lock.release()
-
-    def read(self) :
-        self.read_lock.acquire()
-        frame = self.frame.copy()
-        self.read_lock.release()
-        return frame
-
-    def stop(self) :
-        self.started = False
-        self.thread.join()
-
-    def __exit__(self, exc_type, exc_value, traceback) :
-        self.stream.release()
-
-class StreamServer():
+class StreamServer:
     def __init__(self, host=None, port=10001):
+        self.thread = Thread(target=self.get_items, args=())
         self.host = host
         self.port = port
         self.started = False
@@ -126,7 +67,6 @@ class StreamServer():
             print("already started")
             return None
         self.started = True
-        self.thread = Thread(target=self.get_items, args=())
         self.thread.start()
 
     def get_items(self):
@@ -141,7 +81,7 @@ class StreamServer():
             if data and len(data) > 0:
                 self.current_item = json.loads(data)
             self.update_lock.release()
-    
+
     def ack_item(self, gesture_in):
         print("acking item...")
         self.sock.sendall(gesture_in)
@@ -153,56 +93,141 @@ class StreamServer():
         self.thread.join()
 
 
+class WebcamVideoStream:
+    def __init__(self, src=0, width=320, height=240):
+        self.thread = Thread(target=self.update, args=())
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.started = False
+        self.read_lock = Lock()
+
+    def start(self):
+        if self.started:
+            print("already started!!")
+            return None
+        self.started = True
+        self.thread.start()
+        return self
+
+    def update(self):
+        while self.started:
+            (grabbed, frame) = self.stream.read()
+            self.read_lock.acquire()
+            self.grabbed, self.frame = grabbed, frame
+            self.read_lock.release()
+
+    def read(self):
+        self.read_lock.acquire()
+        frame = self.frame.copy()
+        self.read_lock.release()
+        return frame
+
+    def stop(self):
+        self.started = False
+        self.thread.join()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stream.release()
+
+
+def find_face():
+    try:
+        # find the face in the image
+        face_found = False
+        frame_num = 0
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        while frame_num < 30:
+            # Take first frame and find corners in it
+            frame_num += 1
+            ret, frame = cap.read()
+            global frame_gray
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # faces = face_cascade.detectMultiScale(frame_gray, 1.3, 5)
+            faces = face_cascade.detectMultiScale(frame_gray,
+                                                  scaleFactor=1.1,
+                                                  minNeighbors=5,
+                                                  minSize=(30, 30),
+                                                  flags=cv2.CASCADE_SCALE_IMAGE)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                face_found = True
+            # cv2.imshow('image', frame)
+            cv2.imwrite('face.jpg', frame)
+            cv2.waitKey(1)
+        global face_center
+        face_center = x + w / 2, y + h / 3
+        global p0
+        p0 = np.array([[face_center]], np.float32)
+    except:
+        sys.exit(1)
+
+
 def take_action(gesture, current_item):
     if handling_action:
         return
     print(gesture, current_item)
 
-if __name__ == "__main__" :
-    print("attemping to find face in video....")
+
+def detect_emotions():
+    attr = "age,gender,emotion"
+    detected_faces = CF.face.detect('./face.jpg', attributes=attr)
+    if not detected_faces:
+        print("could not find face")
+        sys.exit(1)
+    for face in detected_faces:
+        print(face)
+
+
+if __name__ == "__main__":
+    print("attempting to find face in video...")
     find_face()
     vs = WebcamVideoStream().start()
     ss = StreamServer("localhost")
     ss.start()
     print("Face center: {}".format(p0))
     f_counter = 1
-    while True :        
+    detect_emotions()
+    while True:
         frame = vs.read()
         old_gray = frame_gray.copy()
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
         if not err:
             sys.exit(1)
-        cv2.circle(frame, get_coords(p1), 4, (0,0,255), -1)
-        cv2.circle(frame, get_coords(p0), 4, (255,0,0))
+        cv2.circle(frame, get_coords(p1), 4, (0, 0, 255), -1)
+        cv2.circle(frame, get_coords(p0), 4, (255, 0, 0))
 
-        #get the xy coordinates for points p0 and p1
-        a,b = get_coords(p0), get_coords(p1)
-        x_movement += abs(a[0]-b[0])
-        y_movement += abs(a[1]-b[1])
+        # get the xy coordinates for points p0 and p1
+        a, b = get_coords(p0), get_coords(p1)
+        x_movement += abs(a[0] - b[0])
+        y_movement += abs(a[1] - b[1])
         text = 'x_movement: ' + str(x_movement)
-        if not gesture: cv2.putText(frame,text,(50,50), font, 0.8,(0,0,255),2)
+        if not gesture: cv2.putText(frame, text, (50, 50), font, 0.8, (0, 0, 255), 2)
         text = 'y_movement: ' + str(y_movement)
-        if not gesture: cv2.putText(frame,text,(50,100), font, 0.8,(0,0,255),2)
+        if not gesture: cv2.putText(frame, text, (50, 100), font, 0.8, (0, 0, 255), 2)
 
         if x_movement > gesture_threshold:
             gesture = 'No'
         if y_movement > gesture_threshold:
             gesture = 'Yes'
         if gesture and gesture_show > 0:
-            cv2.putText(frame,'Gesture Detected: ' + gesture,(50,50), font, 1.2,(0,0,255),3)
+            cv2.putText(frame, 'Gesture Detected: ' + gesture, (50, 50), font, 1.2, (0, 0, 255), 3)
             take_action(gesture, ss.current_item)
             if gesture_show == 10:
                 gesture_lock.acquire()
                 handling_action = True
                 ss.ack_item(gesture)
                 gesture_lock.release()
-            gesture_show -=1
+            gesture_show -= 1
         if gesture_show == 0:
             gesture = False
             x_movement = 0
             y_movement = 0
-            gesture_show = 10 #number of frames a gesture is shown
+            gesture_show = 10  # number of frames a gesture is shown
             if gesture_show == 10:
                 gesture_lock.acquire()
                 handling_action = False
@@ -218,7 +243,7 @@ if __name__ == "__main__" :
             flags=cv2.CASCADE_SCALE_IMAGE)
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
--- ncr_proxy.py
+++ ncr_proxy.py
@@ -3,12 +3,25 @@ import sys
 import json
 import requests
 import socket
-import time
-import random
 import numpy as np
+from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
 
-GATEWAY_URL = "https://gateway-staging.ncrcloud.com"
+load_dotenv()
+catalog = None
+suggestions = None
+GATEWAY_URL = os.getenv('NCR_GATEWAY_ENDPOINT')
+
+payload_false = "{\"availableForSale\":false}"
+payload_true = "{\"availableForSale\":true}"
+
+endpoint_suggestions = "/catalog/items/suggestions?"
+endpoint_available = "/ias/item-availability/"
+endpoint_put = "/ias/item-availability/"
+endpoint_get = "/catalog/items/snapshot/"
+endpoint_snapshots = "/catalog/items/snapshot"
+
+query = "descriptionPattern=corn+flakes"
 
 # authentication used for NCR API
 auth = HTTPBasicAuth('acct:bizniz@biznizserviceuser', 'hackgsuwinner')
@@ -27,31 +40,21 @@ headersIAS = {
     'nep-enterprise-unit': '748c4a76cd714502b3bff0525cb48ca7'
 }
 
-catalog = None
-suggestions = None
 
-def prepData():
-    data = None
-    endpoint_put = "/ias/item-availability/"
-    endpoint_get = "/catalog/items/snapshot/"
-    payload_false = "{\"availableForSale\":false}"
-    payload_true = "{\"availableForSale\":true}"
-    
+def prep_data():
     r_get = requests.get(GATEWAY_URL + endpoint_get, auth=auth, headers=headers)
-
     data = json.loads(r_get.content)
-    
     for item in data['snapshot']:
         product_id = item['itemId']['itemCode']
-        if np.random.choice(2,1) == 2:
-            r_put = requests.put(GATEWAY_URL + endpoint_put + product_id, auth=auth, headers=headersIAS, data=payload_true)
+        if np.random.choice(2, 1) == 2:
+            r_put = requests.put(GATEWAY_URL + endpoint_put + product_id, auth=auth, headers=headersIAS,
+                                 data=payload_true)
         else:
-            r_put = requests.put(GATEWAY_URL + endpoint_put + product_id, auth=auth, headers=headersIAS, data=payload_false)
+            r_put = requests.put(GATEWAY_URL + endpoint_put + product_id, auth=auth, headers=headersIAS,
+                                 data=payload_false)
+
 
 def get_suggestions():
-    # hitting this endpoint
-    endpoint_suggestions = "/catalog/items/suggestions?"
-    query = "descriptionPattern=corn+flakes"
     # Connecting to the API and returning json based on query
     r = requests.get(GATEWAY_URL + endpoint_suggestions + query, auth=auth, headers=headers)
     print(r.content)
@@ -67,13 +70,12 @@ def get_suggestions():
     itemId = item['itemId']
     print(itemId)
 
-    status = isAvailable(itemId)
+    status = is_available(itemId)
 
     print(status)
 
+
 def get_catalog():
-    # hitting this endpoint
-    endpoint_snapshots = "/catalog/items/snapshot"
     # connecting to the API and returning json based on query
     r = requests.get(GATEWAY_URL + endpoint_snapshots, auth=auth, headers=headers)
     print(r.content)
@@ -84,18 +86,16 @@ def get_catalog():
         catalog = json.loads(r.content)
     # sendData(catalog)
 
-def isAvailable(itemId):
-    # hitting this endpoint
-    endpoint_available = "/ias/item-availability/"
+
+def is_available(item_id):
     # request availability of this itemID
-    product_id = itemId.get('itemCode')
+    product_id = item_id.get('itemCode')
     # connecting to the API and returning json based on query
     r = requests.get(GATEWAY_URL + endpoint_available + product_id, auth=auth, headers=headersIAS)
     print(r.content)
     if r.status_code > 400:
         return None
     else:
-        isItThere = None
         isItThere = json.loads(r.content)
 
     if isItThere['availableForSale']:
@@ -103,7 +103,8 @@ def isAvailable(itemId):
     else:
         return False
 
-def sendData(data):
+
+def send_data(data):
     isSnapshot = 'snapshot' in data
 
 
@@ -116,8 +117,9 @@ if __name__ == '__main__':
         conn, addr = sock.accept()
         while True:
             for snap in catalog['snapshot']:
-                is_available = isAvailable(snap['itemId'])
-                resp = json.dumps({"name": snap['shortDescription']['values'][0]['value'], "itemId": snap['itemId'], "is_available": is_available})
+                availability = is_available(snap['itemId'])
+                resp = json.dumps({"name": snap['shortDescription']['values'][0]['value'], "itemId": snap['itemId'],
+                                   "is_available": availability})
                 # print(resp)
                 try:
                     conn.sendall(resp)
